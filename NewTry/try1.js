c4a478b8ff07d621dbe79b04d51614036fc2c132
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-12 18:45:54 +0600

    Hero Section Almost done need revision

--- NewTry/try1.js
+++
@@ -1,8 +0,0 @@
-const menu = document.getElementById("menu");
-const menuItems = Array.from(document.getElementsByClassName("menu-item"));
-
-menuItems.forEach((item, index) => {
-  item.addEventListener("mouseover", () => {
-    menu.dataset.activeIndex = index;
-  });
-});
